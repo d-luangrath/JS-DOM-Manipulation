6ff229d108ddacf08cc54fd9cf2feebdaaf82156
Darla Luangrath <darla.luangrath@gmail.com>
2024-06-18 16:36:04 -0700

    Apply colors to the card

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-console.log("Hello index.js");
-
 const idInput = document.querySelector('#idInput');
 const colorInput = document.querySelector('#colorInput');
 
-console.log(idInput);
-console.log(colorInput);
-
-function setCard(){
-    const card = document.querySelector(`#${idInput.value}`);
-    console.log(card);
+function setCard() {
+  const card = document.querySelector(`#${idInput.value}`);
+  card.style.color = colorInput.value
 }
 
-document.querySelector('#editCardButton').addEventListener('click', setCard)
+document.querySelector("#editCardButton").addEventListener('click', setCard);
+
