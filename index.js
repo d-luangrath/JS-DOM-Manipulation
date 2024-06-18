9fb070bb085a75d11d88d507f69e4a952d22c560
Darla Luangrath <darla.luangrath@gmail.com>
2024-06-18 16:20:54 -0700

    Target each card with the input fields that were created

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const colorInput = document.querySelector('#colorInput');
 
 console.log(idInput);
 console.log(colorInput);
+
+function setCard(){
+    const card = document.querySelector(`#${idInput.value}`);
+    console.log(card);
+}
+
+document.querySelector('#editCardButton').addEventListener('click', setCard)
